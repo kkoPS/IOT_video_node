c7ad6e1b3c7025c31bc418425c110ba004cf39f8
Joel Schär <joel.schar@heig-vd.ch>
2019-03-21 16:44:33 +0100

    setup basic restful api with flask for the client

--- MS/client.py
+++ MS/client.py
@@ -1,20 +1,30 @@
-import socket
+#!/usr/bin/env python3
 
-class ConnectionHandler:
-    """class handling connection"""
+from flask import Flask, json
+from flask_restful import Resource, Api
 
-    def __init__(self, port):
-        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+app = Flask(__name__)
+api = Api(app)
 
-    def send(self):
-        pass
+class base(Resource):
+  
+  def get(self):
+    response = app.response_class(
+      response=json.dumps({'routes':{'/':'base', '/config':'change config'}}),
+      status=200,
+      mimetype='application/json'
+    )
+    return response
 
-    def receive(self):
-        pass
+class config(Resource):
+  """interracting with configuration file"""
+
+  def get(self):
+    return 'hello'
+
+api.add_resource(base, '/')
+api.add_resource(config, '/config')
 
 if __name__ == "__main__":
-
-    connection = ConnectionHandler(8090)
-
-
+    app.run(port='2000')
 
